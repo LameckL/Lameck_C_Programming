b1eb787e4d2965571c9fdfebd132b0cd23f8f73b
Lameck Kipruto <lameckkipruto1@.com>
2023-07-30 02:50:12 -0700

    simple calc fix

--- control_flow/calculator.c
+++ control_flow/calculator.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 /*
  * main - entry point
+ *
+ *Description:
+ * This program is a calculator that carries out mathematical calculations;
+ * adding, subtracting, multiplying and dividing.
+ * It allows users to input two numbers and an operator
+ * of choice and then shows the outcome of the calculation.
  * Return: 0 - success
  *
  */
